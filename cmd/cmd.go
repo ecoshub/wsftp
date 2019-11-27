a1d10a921588b25e8b311c25c5c26acd28737bb2
emre ocak <emrecanocak@windowslive.com>
2019-11-28 02:06:03 +0300

    send/receive data info complication solved for all functions

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -19,83 +19,94 @@ const (
     // 10004 reserved for ws msg
 )
 
-var myIP string = utils.GetInterfaceIP().String()
+var (
+    myIP string = utils.GetInterfaceIP().String()
+    myUsername string = utils.GetUsername()
+    myMAC string = utils.GetEthMac()
+)
 
 func SendRequest(ip, dir , mac , username string){
     fileSize := utils.GetFileSize(dir)
     fileName := utils.GetFileName(dir)
     fileType := utils.GetFileExt(fileName)
-	myUsername := utils.GetUsername()
-    myMAC := utils.GetEthMac()
-    data := fmt.Sprintf(`"username":"%v","ip":"%v","mac":"%v","dir":"%v","fileName":"%v","fileType":"%v","fileSize":"%v","contentType":"file"}`,
-     myUsername, myIP, myMAC, dir, fileName, fileType, strconv.FormatInt(fileSize, 10))
-    rreq := `{"stat":"rreq",` + data
-
-    data = fmt.Sprintf(`"username":"%v","ip":"%v","mac":"%v","dir":"%v","fileName":"%v","fileType":"%v","fileSize":"%v","contentType":"file"}`,
-     username, ip, mac, dir, fileName, fileType, strconv.FormatInt(fileSize, 10))
-    sreq := `{"stat":"sreq",` + data
-    freq := `{"stat":"freq",` + data
-    res := SendMsg(ip, SRLISTEN, rreq)
-    if res {
-        SendMsg(myIP, SRLISTEN, sreq)
-    }else{
-        SendMsg(myIP, SRLISTEN, freq)
-    }
-}
-
-func SendAccept(ip, dir , dest string, port int){
-    username := utils.GetUsername()
-    fileName := utils.GetFileName(dir)
-    fileType := utils.GetFileExt(fileName)
-    data := fmt.Sprintf(`"username":"%v","ip":"%v","dir":"%v","fileName":"%v","fileType":"%v","destination":"%v","port":"%v","contentType":"file"}`,
-        username, ip, dir, fileName, fileType, dest, strconv.Itoa(port))
-    racp := `{"stat":"racp",` + data
-    sacp := `{"stat":"sacp",` + data
-    facp := `{"stat":"facp",` + data
     
-    res := SendMsg(ip, MAINLISTEN, racp)
+    dataToSend := fmt.Sprintf(`"username":"%v","ip":"%v","mac":"%v","dir":"%v","fileName":"%v","fileType":"%v","fileSize":"%v","contentType":"file"}`,
+     myUsername, myIP, myMAC, dir, fileName, fileType, strconv.FormatInt(fileSize, 10))
+    dataToMe := fmt.Sprintf(`"username":"%v","ip":"%v","mac":"%v","dir":"%v","fileName":"%v","fileType":"%v","fileSize":"%v","contentType":"file"}`,
+     username, ip, mac, dir, fileName, fileType, strconv.FormatInt(fileSize, 10))
+
+    rreq := `{"cmd":"rreq",` + dataToSend
+    sreq := `{"cmd":"sreq",` + dataToMe
+    freq := `{"cmd":"freq",` + dataToMe
+    res := TransmitData(ip, SRLISTEN, rreq)
     if res {
-        SendMsg(myIP, SRLISTEN, sacp)
-        SendMsg(ip, SRLISTEN, racp)
+        TransmitData(myIP, SRLISTEN, sreq)
     }else{
-        SendMsg(ip, SRLISTEN, facp)
-        SendMsg(myIP, SRLISTEN, facp)
+        TransmitData(myIP, SRLISTEN, freq)
     }
 }
 
-func SendReject(ip, dir string){
-    username := utils.GetUsername()
+func SendAccept(ip, mac, dir, dest, username, id string, port int){
     fileName := utils.GetFileName(dir)
     fileType := utils.GetFileExt(fileName)
-    data := fmt.Sprintf(`"username":"%v","ip":"%v","dir":"%v","fileName":"%v","fileType":"%v","contentType":"file"}`,
-     username, ip, dir, fileName, fileType)
-    rrej := `{"stat":"rrej",` + data
-    srej := `{"stat":"srej",` + data
-    frej := `{"stat":"frej",` + data
-    res := SendMsg(ip, SRLISTEN, rrej)
+
+    dataToSend := fmt.Sprintf(`"username":"%v","ip":"%v","mac":"%v","dir":"%v","fileName":"%v","fileType":"%v","destination":"%v","port":"%v","id":"%v","contentType":"file"}`,
+        myUsername, myIP, myMAC, dir, fileName, fileType, dest, strconv.Itoa(port), id)
+    dataToMe := fmt.Sprintf(`"username":"%v","ip":"%v","mac":"%v","dir":"%v","fileName":"%v","fileType":"%v","destination":"%v","port":"%v","id":"%v","contentType":"file"}`,
+        username, ip, mac, dir, fileName, fileType, dest, strconv.Itoa(port), id)
+
+    racp := `{"cmd":"racp",` + dataToSend
+    sacp := `{"cmd":"sacp",` + dataToMe
+    facp := `{"cmd":"facp",` + dataToMe
+    
+    res := TransmitData(ip, MAINLISTEN, racp)
     if res {
-    	SendMsg(myIP, SRLISTEN, srej)
+        TransmitData(myIP, SRLISTEN, sacp)
+        TransmitData(ip, SRLISTEN, racp)
     }else{
-    	SendMsg(myIP, SRLISTEN, frej)
+        TransmitData(ip, SRLISTEN, facp)
+        TransmitData(myIP, SRLISTEN, facp)
     }
 }
 
-func SendMessage(ip, to, msg string){
-    username := utils.GetUsername()
-    data := fmt.Sprintf(`"person":"%v","content":"%v","contentType":"text"}`, username, msg)
-    data2 := fmt.Sprintf(`"person":"%v","content":"%v","contentType":"text"}`, to, msg)
-    rmsg := `{"stat":"rmsg",` + data
-    smsg := `{"stat":"smsg",` + data2
-    fmsg := `{"stat":"fmsg",` + data2
-    res := SendMsg(ip,MSGLISTEN, rmsg)
+func SendReject(ip, mac, dir, username string){
+    fileName := utils.GetFileName(dir)
+    fileType := utils.GetFileExt(fileName)
+
+    dataToSend := fmt.Sprintf(`"username":"%v","ip":"%v","mac":"%v","dir":"%v","fileName":"%v","fileType":"%v","contentType":"file"}`,
+     myUsername, myIP, myMAC, dir, fileName, fileType)
+    dataToMe := fmt.Sprintf(`"username":"%v","ip":"%v","mac":"%v","dir":"%v","fileName":"%v","fileType":"%v","contentType":"file"}`,
+     username, ip, mac, dir, fileName, fileType)
+
+    rrej := `{"cmd":"rrej",` + dataToSend
+    srej := `{"cmd":"srej",` + dataToMe
+    frej := `{"cmd":"frej",` + dataToMe
+
+    res := TransmitData(ip, SRLISTEN, rrej)
     if res {
-        SendMsg(myIP, MSGLISTEN, smsg)
+    	TransmitData(myIP, SRLISTEN, srej)
     }else{
-        SendMsg(myIP, MSGLISTEN, fmsg)
+    	TransmitData(myIP, SRLISTEN, frej)
     }
 }
 
-func SendMsg(ip string, port int, msg string) bool{
+func SendMessage(ip, mac, username, msg string){
+    dataToSend := fmt.Sprintf(`"mac":"%v","person":"%v","content":"%v","contentType":"text"}`, myMAC, myUsername, msg)
+    dataToMe := fmt.Sprintf(`"mac":"%v","person":"%v","content":"%v","contentType":"text"}`,mac,  username, msg)
+
+    rmsg := `{"cmd":"rmsg",` + dataToSend
+    smsg := `{"cmd":"smsg",` + dataToMe
+    fmsg := `{"cmd":"fmsg",` + dataToMe
+
+    res := TransmitData(ip,MSGLISTEN, rmsg)
+    if res {
+        TransmitData(myIP, MSGLISTEN, smsg)
+    }else{
+        TransmitData(myIP, MSGLISTEN, fmsg)
+    }
+}
+
+func TransmitData(ip string, port int, msg string) bool{
     strPort := strconv.Itoa(port)
     addr := ip + ":" + strPort
     tcpAddr, err := net.ResolveTCPAddr("tcp", addr)
