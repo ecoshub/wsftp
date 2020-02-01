542a7f46718d670ddac836938f2ac9ad9fd2f30c
eco9999 <emrecanocak@windowslive.com>
2020-02-01 12:55:11 +0300

    name change

--- tools/file_op.go
+++ tools/file_op.go
@@ -1,49 +1,57 @@
 package tools
 
 import (
-	"os"
 	"fmt"
-	"strings"
 	"github.com/ecoshub/penman"
+	"os"
+	"strings"
 )
 
-func GetFileSize(dir string) int64{
-    info, err := os.Stat(dir)
-    if err != nil {
-        return int64(0)
-    }
-    return info.Size()
+func GetFileSize(dir string) int64 {
+	info, err := os.Stat(dir)
+	if err != nil {
+		return int64(0)
+	}
+	return info.Size()
 }
 
-func GetFileName(dir string) string{
-    tokens := strings.Split(dir, SEPARATOR)
-    name := tokens[len(tokens) - 1]
-    return name
+func GetFileName(dir string) string {
+	tokens := strings.Split(dir, SEPARATOR)
+	name := tokens[len(tokens)-1]
+	return name
 }
 
-func GetFileExt(dir string) string{
-    tokens := strings.Split(dir, ".")
-    ext := strings.Join(tokens[1:], ".")
-    return ext
+func GetFileExt(dir string) string {
+	tokens := strings.Split(dir, ".")
+	ext := strings.Join(tokens[1:], ".")
+	return ext
 }
 
-func UniqName(dest, fileName string, filesize int64) string{
-    if !penman.IsFileExist(dest + SEPARATOR + fileName){
-        return fileName
-    }
-    tokens := strings.Split(fileName, ".")
-    name := tokens[0]
-    ext := strings.Join(tokens[1:], ".")
-    count := 1
-    for {
-        newName := fmt.Sprintf("%v(%v).%v", name, count, ext)
-        if penman.IsFileExist(dest + SEPARATOR + newName){
-            if GetFileSize(dest + SEPARATOR + newName) < filesize {
-                return newName
-            }
-        }else{
-            return newName
-        }
-        count++
-    }
+func UniqName(dest, fileName string, filesize int64) string {
+	if !penman.IsFileExist(dest + SEPARATOR + fileName) {
+		return fileName
+	}
+	tokens := strings.Split(fileName, ".")
+	name := tokens[0]
+	ext := strings.Join(tokens[1:], ".")
+	count := 1
+	for {
+		newName := fmt.Sprintf("%v(%v).%v", name, count, ext)
+		if penman.IsFileExist(dest + SEPARATOR + newName) {
+			if GetFileSize(dest+SEPARATOR+newName) < filesize {
+				return newName
+			}
+		} else {
+			return newName
+		}
+		count++
+	}
+}
+
+func GetPackNumber(totalsize, speed int64) int {
+	totalFrag := (totalsize / speed)
+	if float64(totalFrag) < (float64(totalsize) / float64(speed)) {
+		totalFrag++
+	}
+	return int(totalFrag)
 }
